4d0af5245715a4ea3cfca7f305c791c8d9a56d8b
CryTyme <34171378+CryTyme@users.noreply.github.com>
2019-05-14 15:49:54 -0400

    Build on Nodes
    Added ability to build directly onto the nodes with a standard turret

---
+++ TowerDefense01/Assets/Scripts/Node.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset; //because the turret spawns inside node 
+
+
+    private GameObject turret;
+
+    private Renderer rend;
+
+    private Color startColor;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+
+    private void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Can't Build There - TODO DISPLAY ON SCREEN");
+            return;
+        }
+
+        //build a turret
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject) Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+
+    }
+
+    void OnMouseEnter() // Mouse over highlight 
+    {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
