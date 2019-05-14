01b51d7444e64d3d064382ae6343c66611bc4ca8
CryTyme <34171378+CryTyme@users.noreply.github.com>
2019-05-13 20:32:40 -0400

    Added Targeting for Turrets
    Added in turret and targeting calculations

---
+++ TowerDefense01/Assets/Scripts/Turret.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+
+{
+
+    private Transform target;
+    public float range = 15f;
+
+    public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
+    public float turnSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f); //repeats update target 2x a second
+    }
+    
+    void UpdateTarget () //renewed search for new target
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag); //targets for enemies marked as such
+        float shortestDistance = Mathf.Infinity; //creates float for near enemy but defaults to infinite range
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        } else
+        {
+            target = null;
+        }
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+            return;
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles; //smooth out targeting
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    private void OnDrawGizmosSelected() //draws range for turret if selected
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
---
+++ TowerDefense01/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //allows for ui manipulation
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f; //creates countdown float
+
+    public Text waveCountDownText;
+
+    private int waveIndex = 0;
+
+    void Update()
+    {
+        if (countdown <= 0f) //coutdown to zero and then announcewave 
+        {
+            StartCoroutine(SpawnWave());
+          countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+
+        waveCountDownText.text = Mathf.Round(countdown).ToString(); //cuts off decimal places
+    }
+
+    IEnumerator SpawnWave ()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+            
+        }
+
+        
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
